a229070767fd785754cee92a7ddd47815345b862
Chodongdong <117080299+Chodongdong@users.noreply.github.com>
2026-03-24 20:54:58 +0900

    [level 1] Title: 문자열 나누기, Time: 25.92 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -0,0 +1,22 @@
+function solution(s) {
+    let stack = [];
+    let answer = 0;
+    
+    for(let i = 0; i < s.length; i += 1){
+        stack.push(s[i]);
+        
+        const same = stack.filter((item) => item === stack[0]);
+        const notSame = stack.filter((item) => item !== stack[0]);
+        
+        if(same.length === notSame.length){
+            answer += 1;
+            stack = [];
+        }
+    }
+    
+    if(stack.length !== 0){
+        answer += 1;
+    }
+    
+    return answer;
+}
