1199ea7ecab4dec10a5facde98825f05b3c92a09
Chodongdong <117080299+Chodongdong@users.noreply.github.com>
2026-04-05 15:27:10 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -0,0 +1,23 @@
+function solution(new_id) {
+ let answer = new_id;
+
+  answer = answer.toLowerCase(); 
+  answer = answer.replace(/[^a-z0-9-_.]/g, ''); 
+  answer = answer.replace(/\.+/g, '.'); 
+  answer = answer.replace(/^\.|\.$/g, ''); 
+
+  if (answer === '') {
+    answer += 'a';
+  }
+
+  if (answer.length >= 16) {
+    answer = answer.substring(0, 15);
+  }
+  answer = answer.replace(/\.$/g, '');
+
+  while (answer.length <= 2) {
+    answer += answer.charAt(answer.length - 1);
+  }
+
+  return answer;
+}
